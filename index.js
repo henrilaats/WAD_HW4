87f6c589ca04bb205734baab57dc95a29c831f82
henrilaats <henrilaats55@gmail.com>
2021-12-01 16:38:03 +0200

    created the project structure for hw4

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.listen(3000);
+app.use(express.static('Public'));
+
+app.use((req, res, next) => {
+    console.log('a new request was made');
+    next();
+});
+
+app.get('/', (req, res) => {
+    let posts = [
+        {id:1, title: "books titles", body: "description of the book"}
+    ];
+    res.render('posts', {posts: posts})
+});
+
+app.get('/addnewpost', (reg, res) => {
+    res.render('addnewpost')
+})
+
+app.get('/singlepost', (reg, res) => {
+    res.render('singlepost')
+})
+
+app.use((req, res) => {
+ res.render('404').render('404');
+});
