4cf2761d4848c3dc524a100eae62f0c2a553a77e
henrilaats <henrilaats55@gmail.com>
2021-12-03 15:45:59 +0200

    Added singlepost page and delete function

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const pool = require('./database');
-const cors = require('cors');
+const express = require("express");
+const pool = require("./database");
+const cors = require("cors");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static('Public'));
+app.use(express.static("Public"));
 
 app.listen(3000, () => {
-    console.log("Server is listening to port 3000")
+  console.log("Server is listening to port 3000");
 });
 
 app.use((req, res, next) => {
-    console.log('a new request was made');
-    next();
+  console.log("a new request was made");
+  next();
 });
 
 /*
@@ -30,28 +30,54 @@ app.get('/', (req, res) => {
     res.render('posts', {posts: posts})
 });*/
 
-
-app.get('/', async(req, res) => {
-    try {
+app.get("/", async (req, res) => {
+  try {
     console.log("get posts request has arrived");
     const posts = await pool.query("SELECT * FROM posts");
-    res.render('posts', {posts: posts.rows});
+    res.render("posts", { posts: posts.rows });
     /*res.json(posts.rows);*/
-    } catch (err) {
+  } catch (err) {
     console.error(err.message);
-    }
-   });
+  }
+});
 
-   
-   
-app.get('/addnewpost', (reg, res) => {
-    res.render('addnewpost')
-})
+app.get("/addnewpost", (reg, res) => {
+  res.render("addnewpost");
+});
+/*
+app.get("/singlepost", (reg, res) => {
+  res.render("singlepost");
+});*/
+app.get("/singlepost/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    console.log(req.params.id);
+    console.log("get a single post request has arrived");
+    const posts = await pool.query("SELECT * FROM posts WHERE id = $1", [
+      id,
+    ]);
+    res.render("singlepost", { posts: posts.rows[0] });
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
-app.get('/singlepost', (reg, res) => {
-    res.render('singlepost')
-})
+app.delete('/posts/:id', async(req, res) => {
+  try {
+  console.log(req.params);
+  const { id } = req.params;
+  const post = req.body;
+  console.log("delete a post request has arrived");
+  const deletepost = await pool.query(
+  "DELETE FROM posts WHERE id = $1", [id]
+  );
+  res.redirect('posts');
+  } catch (err) {
+  console.error(err.message);
+  }
+ });
+ 
 
 app.use((req, res) => {
- res.render('404').render('404');
+  res.render("404").render("404");
 });
