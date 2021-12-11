4084a50b25364edbc428da54398b3cddc1514270
GloriaKrieger <gloria.krieger123@gmail.com>
2021-12-11 13:26:17 +0200

    Readme, autogenerate, style, author

--- README.md
+++ README.md
@@ -4,3 +4,19 @@ Team members:
 * Gloria Krieger,
 * Henri Laats,
 * Paule Leslie Kuipou
+
+How to run:
+1) Create database in Postgres
+2) Create Table in Postgres Query Editor:
+   
+
+   CREATE TABLE posts (
+   id SERIAL,
+   title varchar(255) NOT NULL,
+   body varchar(255) NOT NULL,
+   likes int NOT NULL,
+   author varchar(255) NOT NULL
+   );
+
+
+3) In database.js change password, database and username.
--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-const express = require('express');
-const pool = require('./database');
-const cors = require('cors');
-const uuid = require("uuid");
+const express = require("express");
+const pool = require("./database");
+const cors = require("cors");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static('Public'));
+app.use(express.static("Public"));
+
+app.listen(3000, () => {
+    console.log("Server is listening to port 3000");
+});
+
+app.use((req, res, next) => {
+    console.log("a new request was made");
+    next();
+});
 
 /*
 app.get('/', (req, res) => {
@@ -22,73 +30,36 @@ app.get('/', (req, res) => {
     res.render('posts', {posts: posts})
 });*/
 
-
-// List of all posts in database
-app.get('/', async(req, res) => {
+app.get("/", async (req, res) => {
     try {
         console.log("get posts request has arrived");
         const posts = await pool.query("SELECT * FROM posts");
-        res.render('posts', {posts: posts.rows});
+        res.render("posts", {posts: posts.rows});
         /*res.json(posts.rows);*/
     } catch (err) {
         console.error(err.message);
     }
 });
-  
-// Add a new post
-app.get('/addnewpost', (reg, res) => {
-    res.render('addnewpost')
+
+app.get("/addnewpost", (reg, res) => {
+    res.render("addnewpost");
 });
-
-// Insert a new post in database
-app.post('/addnewpost', (req, res) => {
-    try {
-        console.log("Adding a post in database");
-        var title = req.body.title;
-        var author = req.body.author;
-        var content = req.body.content;
-        var link = req.body.link;
-        var params = [title, author, content, link];
-
-        var queryString = `INSERT INTO posts (title, content, 
-            author, link) VALUES ($1, $2, $3, $4)`;
-
-        // Querying the database
-        pool.connect(function(err, client){
-            if(err) {
-                console.log(err.message);
-                return res.send('error');
-            }
-            client.query(queryString, params, function(err) {
-                if (err) {
-                    console.log(err.message);
-                } else {
-                    console.log(res.body);
-                    // Once the query is done, redirect to the list
-                    // of posts to see the changes
-                    res.redirect('/');
-                }
-            });
-        });
-    } catch (err) {
-        console.log(err.message);
-    }
-});
-
-// Render a single post based on its ID
+/*
+app.get("/singlepost", (reg, res) => {
+  res.render("singlepost");
+});*/
 app.get('/post/:id', async (req, res) => {
     try {
         console.log("Get single post request has arrived");
         postId = req.params.id;
-        const post = await pool.query("SELECT * FROM posts WHERE id = "+postId);
+        const post = await pool.query("SELECT * FROM posts WHERE id = " + postId);
         //console.log(post.rows);
         res.render('singlepost', {post: post.rows[0]});
     } catch (err) {
         console.error(err.message);
     }
-});
+})
 
-// Delete a post based on its ID
 app.get('/delete/:id', (req, res) => {
     try {
         postId = req.params.id;
@@ -100,23 +71,32 @@ app.get('/delete/:id', (req, res) => {
     }
 });
 
-// Add a like on a post based on its ID
+app.post('/', async (req, res) => {
+    try {
+        const post = req.body;
+        console.log("POST title: " + post.title + "post body " + post.body + "auth " + post.author);
+        const newpost = await pool.query(
+            "INSERT INTO posts(title, body, likes, author) values ($1, $2, $3, $4) RETURNING*", [post.title, post.body, 0, post.author]
+        );
+        res.redirect('/');
+    } catch (err) {
+        console.error(err.message)
+    }
+});
+
 app.post('/addlike/:id', (req, res) => {
     try {
         console.log("Add like on post request has arrived");
         postId = req.params.id;
         like = req.body.like;
-        pool.query("UPDATE posts SET likes = "+ like +" WHERE id = "+postId);
+        pool.query("UPDATE posts SET likes = " + like + " WHERE id = " + postId);
     } catch (err) {
         console.error(err.message);
     }
 });
 
-// Error page (404)
-app.use((req, res) => {
-    res.render('404');
-});
 
-app.listen(3000, () => {
-    console.log("Server is listening to port 3000")
+app.use((req, res) => {
+    console.log("always here?")
+    res.render("404").render("404");
 });
